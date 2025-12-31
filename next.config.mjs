e1826b705aac599695bf68426f96a4f5d00dcb59
Shajnush Drik <shajnushdrik26@gmail.com>
2025-12-31 21:21:58 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -24,4 +24,21 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default nextConfig;/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "agcs.edu.bd",
+      },
+      {
+        protocol: "https",
+        hostname: "www.agcs.edu.bd",
+      },
+    ],
+  },
+}
+
+export default nextConfig
+

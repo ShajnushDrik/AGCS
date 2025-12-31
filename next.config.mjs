b46eb30e7beeddb2fe95e659a5260d6948f8c942
Shajnush Drik <shajnushdrik26@gmail.com>
2025-12-31 18:33:44 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Allows production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'agcs.edu.bd',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.agcs.edu.bd',
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
